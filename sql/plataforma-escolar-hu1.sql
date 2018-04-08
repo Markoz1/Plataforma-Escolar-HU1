ff3398c4780e532d7f233d5c748eb7796e691baa
markoz1 <marco.jhoel@gmail.com>
2018-04-07 22:33:02 -0400

    script BD update

--- sql/plataforma-escolar-hu1.sql
+++ sql/plataforma-escolar-hu1.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-04-2018 a las 18:28:51
--- Versión del servidor: 10.1.31-MariaDB
+-- Tiempo de generación: 08-04-2018 a las 04:30:32
+-- Versión del servidor: 10.1.31-MariaDBMariaDB
 -- Versión de PHP: 7.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -30,17 +30,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `estudiante` (
   `id_estudiante` int(11) NOT NULL,
-  `apellido_paterno` varchar(255) NOT NULL,
+  `apellido_paterno` varchar(255) DEFAULT NULL,
   `apellido_materno` varchar(255) NOT NULL,
   `nombre` varchar(255) NOT NULL,
-  `pais` varchar(255) NOT NULL,
-  `departamento` varchar(255) NOT NULL,
-  `ci` varchar(255) NOT NULL,
+  `pais` varchar(255) DEFAULT NULL,
+  `departamento` varchar(255) DEFAULT NULL,
+  `ci` varchar(255) DEFAULT NULL,
   `fecha_nacimiento` date NOT NULL,
   `sexo` varchar(255) NOT NULL,
   `direccion` varchar(255) DEFAULT NULL,
   `telefono` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `estudiante`
+--
+
+INSERT INTO `estudiante` (`id_estudiante`, `apellido_paterno`, `apellido_materno`, `nombre`, `pais`, `departamento`, `ci`, `fecha_nacimiento`, `sexo`, `direccion`, `telefono`) VALUES
+(1, 'apellido1', 'apellido2', 'nombre1', 'bolivia', 'cochabamba', '23123122', '2018-01-11', 'M', 'sdfasdf', '312312334');
 
 --
 -- Índices para tablas volcadas
@@ -60,7 +67,7 @@ ALTER TABLE `estudiante`
 -- AUTO_INCREMENT de la tabla `estudiante`
 --
 ALTER TABLE `estudiante`
-  MODIFY `id_estudiante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_estudiante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
